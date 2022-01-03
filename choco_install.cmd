0f43b7bc9572eb094a85083336fb5827ce865988
rherdman1953 <rherdman@gmail.com>
2022-01-03 11:11:50 -0600

    Add files via upload

---
+++ choco_install.cmd
@@ -0,0 +1,60 @@
+
+
+rem 
+rem @”%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe” -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command “iex ((New-Object System.Net.WebClient).DownloadString(‘https://chocolatey.org/install.ps1'))“ && SET “PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin”
+rem
+rem choco install chocolateygui -fy
+rem
+
+choco install 1password -fy
+
+choco install Firefox -fy
+choco install brave -fy
+
+choco install fsviewer -fy
+choco install greenshot -fy
+
+choco install steam-client-fy
+
+choco install adobereader -fy
+
+choco install dropbox -fy
+choco install filezilla -fy
+
+choco install notepadplusplus -fy
+
+choco install vlc -fy
+choco install filezilla -fy
+choco install handbrake -fy
+
+rem choco install spotify -fy
+
+
+choco install filezilla -fy
+
+choco install qbittorrent -fy
+choco install revo-uninstaller -fy
+
+choco install 7zip -fy
+choco install MobaXTerm -fy
+
+choco install geforce-experience -fy
+choco install geforce-game-ready-driver -fy
+
+
+
+choco install patch-my-pc -fy
+choco install resilio-sync-home -fy
+
+choco install Office365HomePremium -fy
+choco install adobe-creative-cloud -fy
+
+choco install calibre -fy
+
+
+rem missing apps:
+rem verasync
+rem Media Info
+rem MP3Tag
+rem TreeSize pro
+rem Advanced Renamer
