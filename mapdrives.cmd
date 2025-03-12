ad31c38f5e76c6442a5a78817cf3389b8e6e29c6
rherdman1953 <rherdman@gmail.com>
2025-03-12 11:24:56 -0500

    Create mapdrives.cmd

---
+++ mapdrives.cmd
@@ -0,0 +1,12 @@
+net use w: /del
+net use x: /del
+net use y: /del
+net use z: /del
+
+net use w: \\caladan\hbrake %1 /user:rich 
+net use x: \\caladan\foo  
+net use y: \\caladan\img  
+net use z: \\caladan\media 
+
+pause
+
