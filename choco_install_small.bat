2c69b3a91efb4895d700613ed0d6e7ba0dbe6e76
rherdman1953 <rherdman@gmail.com>
2025-03-12 11:24:18 -0500

    Create choco_install_small.bat

---
+++ choco_install_small.bat
@@ -0,0 +1,20 @@
+
+rem Run this in PowerSHell (elevated):
+rem Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+rem pause
+
+rem
+choco install chocolateygui -fy
+rem
+
+
+choco install discord -fy
+
+choco install advanced-renamer -fy
+
+
+choco install samsung-magician -fy
+
+
+choco install hashcheck -fy
+
