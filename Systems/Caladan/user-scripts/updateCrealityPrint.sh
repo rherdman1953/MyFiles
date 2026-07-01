4ef58195c79b27e1b16762d486f2a346a6928a5f
Rich Herdman <rherdman@gmail.com>
2026-07-01 10:31:19 -0500

    Added script to remember best way to update Creality Print appimage

---
+++ Systems/Caladan/user-scripts/updateCrealityPrint.sh
@@ -0,0 +1,6 @@
+chmod +x ~/Downloads/New-CrealityPrint-Version.AppImage
+mv ~/Downloads/New-CrealityPrint-Version.AppImage ~/Applications/CrealityPrint/
+
+ln -sfn \
+  ~/Applications/CrealityPrint/New-CrealityPrint-Version.AppImage \
+  ~/Applications/CrealityPrint/CrealityPrint.AppImage
