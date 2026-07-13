2aee383ca51282cc0683b270808e825cef93d5f8
Rich Herdman <rherdman@gmail.com>
2026-07-13 13:55:12 -0500

    caladan: backup user scripts (2026-07-13 13:55)

--- Systems/Caladan/user-scripts/updateCrealityPrint.sh
+++
@@ -1,6 +0,0 @@
-chmod +x ~/Downloads/New-CrealityPrint-Version.AppImage
-mv ~/Downloads/New-CrealityPrint-Version.AppImage ~/Applications/CrealityPrint/
-
-ln -sfn \
-  ~/Applications/CrealityPrint/New-CrealityPrint-Version.AppImage \
-  ~/Applications/CrealityPrint/CrealityPrint.AppImage
