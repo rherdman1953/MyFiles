bba8b294dbc29f30e5f9895f1642733e4bdad393
rherdman1953 <rherdman@gmail.com>
2026-08-28 14:23:45 -0500

    misc

---
+++ Systems/Windows11/scripts/mapdrives-ix.cmd
@@ -0,0 +1,35 @@
+@echo off
+setlocal EnableExtensions
+
+set "SERVER=caladan"
+set "USER=caladan\ix-user"
+
+REM Clear existing mapped drives
+net use W: /delete /y >nul 2>&1
+net use X: /delete /y >nul 2>&1
+net use Y: /delete /y >nul 2>&1
+net use Z: /delete /y >nul 2>&1
+
+REM Clear all existing sessions to caladan, including IPC$
+net use \\%SERVER%\IPC$ /delete /y >nul 2>&1
+net use \\%SERVER%\* /delete /y >nul 2>&1
+
+REM Force Windows to use the stored Credential Manager password for caladan\rich
+net use \\%SERVER%\IPC$ /user:%USER%
+if errorlevel 1 (
+  echo Failed to authenticate to \\%SERVER% as %USER%.
+  echo Make sure Credential Manager has an entry for %SERVER% using %USER%.
+  pause
+  exit /b 1
+)
+
+REM Persistent mappings reuse the authenticated caladan\rich session
+net use W: "\\%SERVER%\hbrake" /persistent:yes
+net use X: "\\%SERVER%\foo" /persistent:yes
+net use Y: "\\%SERVER%\img" /persistent:yes
+net use Z: "\\%SERVER%\media" /persistent:yes
+
+echo.
+echo Done.
+net use
+pause
